86b07b9336fc6e56ff573258fa4ad9b239581a58
zhackerschoice <team.telegram.org@gmail.com>
2021-08-27 03:49:09 -0700

    update

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,8 +1,9 @@
 #include <stdio.h>
-
-
-int main(void)
-/* This is Description */ 
+/* De
+*scri
+*ptionnnnnnnnnnnnnnnn
+*/
+int main(void) 
 {
 puts("\"Programming is like building a multilingual puzzle");
 return (0);

37423603dfb3132653112c7e48c3a3e2da21667e
zhackerschoice <team.telegram.org@gmail.com>
2021-08-28 11:23:14 -0700

    update

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+* main - Prints a text according number
+*
+* Return: Always (Success)
+*/
+int main(void)
+{
+char alpha;
+char alphaA;
+
+for (alpha = 'a'; alpha <= 'z'; alpha++)
+putchar(alpha);
+putchar('\n');
+for (alphaA = 'A'; alphaA <= 'Z'; alpha++)
+putchar(alphaA);
+putchar('\n');
+return (0);
+}

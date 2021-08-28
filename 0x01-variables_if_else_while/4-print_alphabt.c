5d33a8d10ade317a0086c2ed7783e23a917a3943
zhackerschoice <team.telegram.org@gmail.com>
2021-08-28 12:32:25 -0700

    update

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+char alpha, e, q;
+
+e = 'e';
+q = 'q';
+
+for (alpha = 'a'; alpha <= 'z'; alpha++)
+{
+if (low != e && alpha != q)
+putchar(alpha);
+}
+printf("\n");
+
+return (0);
+}

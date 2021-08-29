56a1ced8f54413bb29e038b54d2abf568683d530
zhackerschoice <team.telegram.org@gmail.com>
2021-08-29 01:01:33 -0700

    update

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -10,6 +10,7 @@ int main(void)
 int num;
 
 for (num = '0'; num <= '9'; num++)
+{
 putchar(num);
 if (num != '9')
 {
@@ -17,5 +18,6 @@ putchar(',');
 putchar(' ');
 }
 putchar('\n');
+}
 return (0);
 }

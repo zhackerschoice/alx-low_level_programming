7c4cd0543c4967ccdeab0d08f03317836761a8d9
zhackerschoice <team.telegram.org@gmail.com>
2021-08-29 00:54:08 -0700

    update

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+/**
+* main - Prints a text according number
+*
+* Return: Always (Success)
+*/
+int main(void)
+{
+int num;
+
+for (num = '0'; num <= '9'; num++)
+putchar(num);
+if (num != '9')
+{
+putchar(',');
+putchar('');
+}
+putchar('\n');
+return (0);
+}

b7f97495585ccf12b61f6ca282d7254e7f3915dd
zhackerschoice <team.telegram.org@gmail.com>
2021-08-31 06:49:19 -0700

    update

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,17 @@
+#include "holberton.h"
+/**
+ * print_alphabet - function to print abc
+ *
+ * Return: 0
+ */
+void print_alphabet(void)
+{
+char alpha;
+
+for (alpha = 'a'; alpha <= 'z'; alpha++)
+_putchar(alpha);
+_putchar('\n');
+return (0);
+}
+int loop;
+for (loop = 0; loop <= 10; loop++)
---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,16 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * _abs - function that computes the absolute value of an integer
+ * @c:  is the int that will use for the argument of the function
+ * Return: 0
+ */
+int _abs(int r)
+{
+	if (r > 0 || n == r)
+	{
+	return (r);
+	}
+	else
+	return (r * -1);
+}

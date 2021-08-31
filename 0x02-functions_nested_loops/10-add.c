2359f9326e08227ee02d9472f319540b7077c5ad
zhackerschoice <team.telegram.org@gmail.com>
2021-08-31 08:13:24 -0700

    update

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,14 @@
+#incude 'main.h'
+
+/**
+ * add - Adds two integers.
+ * @num1: The first integer to be added.
+ * @num2: The second integer to be added.
+ *
+ * Return: The result of the addition.
+ */
+
+int add(n1, n2)
+{
+return (n1 + n2);
+}

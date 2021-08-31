9655487356f443f7651906f839de774d85c365a1
zhackerschoice <team.telegram.org@gmail.com>
2021-08-31 02:05:14 -0700

    update

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,16 @@
+#include "holberton.h"
+/**
+ * print_alphabet - function to print abc
+ *
+ * Return: 0
+ */
+void print_alphabet(void)
+{
+char alpha;
+
+for (alpha = 'a'; alpha <= 'z'; alpha++)
+_putchar(alpha);
+_putchar('\n');
+return (0);
+}
+

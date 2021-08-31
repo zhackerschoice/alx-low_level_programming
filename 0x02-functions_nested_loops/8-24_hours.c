47a937c7bbf1d1cd1b897e837cdca6c9bd88f285
zhackerschoice <team.telegram.org@gmail.com>
2021-08-31 07:23:19 -0700

    update

---
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -0,0 +1,32 @@
+#include "main.h"
+/**
+ * jack_bauer - func that prints every minute of the day of Jack Bauer, starti
+ * n from 00:00 to 23:59, min loop counts mins, while hour loop counts hours
+ * and resets mins
+ * Return: 0
+ */
+void jack_bauer(void)
+{
+	int h = 0;
+	int m = 0;
+	int h2;
+	int m2;
+
+	while (h <= 23)
+	{
+	while (m <= 59)
+	{
+	m2 = minutes % 10;
+	h2 = hours % 10;
+	_putchar(h / 10 + '0');
+	_putchar(h2 + '0');
+	_putchar(':');
+	_putchar(m / 10 + '0');
+	_putchar(m2 + '0');
+	m++;
+	_putchar('\n');
+	}
+	h++;
+	minutes = 0;
+}
+}

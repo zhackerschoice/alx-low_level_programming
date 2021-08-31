a92f6316d1d308ff9f2a7d655e247853a8d852f7
zhackerschoice <83168415+zhackerschoice@users.noreply.github.com>
2021-08-31 10:27:13 -0700

    Update 0-holberton.c

--- 0x02-functions_nested_loops/0-holberton.c
+++ 0x02-functions_nested_loops/0-holberton.c
@@ -7,7 +7,7 @@
  */
 int main(void)
 {
-char holberton[9] = {'H', 'o', 'l', 'b', 'e', 'r', 't', 'o', 'n'};
+char holberton[9] = {'_', 'p', 'u', 't', 'c', 'h', 'a', 'r'};
 unsigned int alpha;
 for (alpha = 0; alpha < sizeof(holberton); alpha++)
 {

b31c78209d930564f9df3a207bc498aa489515fb
zhackerschoice <team.telegram.org@gmail.com>
2021-08-31 07:11:07 -0700

    update

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,18 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * print_last_digit - Prints the last digit of a number.
+ * @n: The number in question.
+ *
+ * Return: Value of the last digit.
+ */
+int print_last_digit(int n)
+{
+int ld = n%10;
+if (n < 0)
+ld *= n;
+
+_putchar('0' + ld);
+return(ld);
+}

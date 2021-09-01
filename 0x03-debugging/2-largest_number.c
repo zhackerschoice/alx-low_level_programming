955176a2231ff78c8a8521d013a58296c9cc40b9
zhackerschoice <team.telegram.org@gmail.com>
2021-09-01 11:08:48 -0700

    update

--- 0x03-debugging/2-largest_number.c
+++ 0x03-debugging/2-largest_number.c
@@ -8,9 +8,9 @@
  * Return: largest number
  */
 
-double largest_number(double a, double b, double c)
+int largest_number(int a, int b, int c)
 {
-double largest;
+int largest;
 
 if (a > b && a > c)
 {

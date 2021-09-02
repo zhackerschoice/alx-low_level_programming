22db62ecc00621c7a20f995d00e7db3d7cb332f0
zhackerschoice <team.telegram.org@gmail.com>
2021-09-02 06:28:52 -0700

    update

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -1,19 +1,19 @@
 #include "main.h"
 #include <ctype.h>
-#include <stdio.h>
+
 /**
  * _isupper - Checks for an uppercase letter
  * @c: The int value of character to evaluate
  *
  * Return: 1 if uppercase, other case 0.
  */
-int _isupper(int c){
-
+int _isupper(int c)
+{
 if (isupper(c))
-return(1);
+return (1);
 
 else
-return(0);
+return (0);
 }
 
 

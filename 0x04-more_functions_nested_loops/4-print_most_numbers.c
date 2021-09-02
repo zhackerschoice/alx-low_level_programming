a6764b199394a685620035f94c75f6c8d29b5b7e
zhackerschoice <team.telegram.org@gmail.com>
2021-09-02 09:58:22 -0700

    update

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -4,13 +4,13 @@
  * print_most_numbers - Print except 2 and 4
  * Return: loop nums except 2 and 4.
  */
-print_most_numbers(void)
+void print_most_numbers(void)
 {
 char n ;
 for (n= '0'; n <= 9; n++)
 if (n!=2 && n!=4)
 {
-_putchar(n)
+_putchar(n);
 }
-_putchar('\n')
+_putchar('\n');
 }

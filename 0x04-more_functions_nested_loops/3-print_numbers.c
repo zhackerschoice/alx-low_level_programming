e3e00a135628d627ce4a219d8bb73b90b040f0a0
zhackerschoice <team.telegram.org@gmail.com>
2021-09-02 09:42:08 -0700

    update

--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -1,12 +1,13 @@
 #include "main.h"
+
 /**
  * print_numbers - function that checks for uppercase character.
  * Return: n
  */
 void print_numbers(void)
 {
-char n;
-n = 0;
+char n = 0;
+;
 while (n <= 9)
 {
 _putchar(n);

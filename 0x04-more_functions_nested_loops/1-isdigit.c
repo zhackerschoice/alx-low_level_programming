eddd33143ed8561bd5b8f0adfc0dedacac2a8ff2
zhackerschoice <team.telegram.org@gmail.com>
2021-09-02 06:59:42 -0700

    update

---
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -0,0 +1,17 @@
+#include "main.h"
+#include <ctype.h>
+#include <stdio.h>
+
+/**
+ * _isdigit - Checks for a digit
+ * @c: The int value of character to evaluate
+ *
+ * Return: 1 if uppercase, other case 0.
+ */
+int _isdigit(int c)
+{
+if (isdigit(c))
+return(1)
+else
+return(0)
+}

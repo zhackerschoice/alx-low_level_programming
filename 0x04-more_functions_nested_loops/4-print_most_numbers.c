8b4275c1203ca96de2ac77656614fd36fcb062b6
zhackerschoice <team.telegram.org@gmail.com>
2021-09-02 09:56:22 -0700

    update

---
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * print_most_numbers - Print except 2 and 4
+ * Return: loop nums except 2 and 4.
+ */
+print_most_numbers(void)
+{
+char n ;
+for (n= '0'; n <= 9; n++)
+if (n!=2 && n!=4)
+{
+_putchar(n)
+}
+_putchar('\n')
+}
--- 0x04-more_functions_nested_loops/main.h
+++ 0x04-more_functions_nested_loops/main.h
@@ -1,10 +1,12 @@
 #ifndef MAIN_H
 #define MAIN_H
 #include <stdio.h>
+
 int _putchar(char);
 int _isupper(int c);
 int _isdigit(int c);
 int mul(int n1,int n2);
 void print_numbers(void);
+void print_most_numbers(void);
 
 #endif /* MAIN_H */

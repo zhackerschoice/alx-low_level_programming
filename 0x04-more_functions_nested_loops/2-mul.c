fa733c31ee3b186e904fad493eb6d0756d3b7afb
zhackerschoice <team.telegram.org@gmail.com>
2021-09-02 07:35:27 -0700

    update

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * mul - Multiplies 2 nums
+ * @n1: number one
+ * @n2: number two
+ * Return: multiplied num.
+ */
+int mul(int n1, int n2)
+{
+return (n1 * n2);
+}
--- 0x04-more_functions_nested_loops/main.h
+++ 0x04-more_functions_nested_loops/main.h
@@ -4,5 +4,6 @@
 
 int _isupper(int c);
 int _isdigit(int c);
+int mul(int n1,int n2);
 
 #endif /* MAIN_H */

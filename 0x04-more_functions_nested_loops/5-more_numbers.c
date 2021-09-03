2a0b733536ba920635edc782b2af0d9615d763f0
zhackerschoice <team.telegram.org@gmail.com>
2021-09-03 11:13:53 -0700

    update

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * more_numbers - print 10 times
+ * Return: loop nums except 2 and 4.
+ */
+void more_numbers(void)
+{
+char n;
+char nn = '0';
+char nnn[2] = {'1', '0'};
+char nnnn[2] = {'1', '4'};
+for (n = '0'; n <= nnn; n++)
+while (nn <= nnnn)
+{
+_putchar(i);
+return(0);
+}
+_putchar('\n')
+}

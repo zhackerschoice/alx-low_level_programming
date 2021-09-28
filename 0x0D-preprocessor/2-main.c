835d1b5349e13e06b1f1740340215d188279e6a5
zhackerschoice <team.telegram.org@gmail.com>
2021-09-28 10:35:21 -0700

    update

--- 0x0D-preprocessor/2-main.c
+++ 0x0D-preprocessor/2-main.c
@@ -1,12 +1,13 @@
 #include <stdio.h>
 
 /**
-*main - prints file name
-*return: the program executed successfully
+* main - prints file name
+*
+* Return: the program executed successfully
 */
 
 int main(void)
 {
-printf ("%s\n" __BASE_FILE__);
+printf("%s\n", __BASE_FILE__);
 return (0);
 }
